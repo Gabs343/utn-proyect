fb6645faea421f99fdeef6efb565e03425ca231a
Gabs343 <gabriellopezp343@gmail.com>
2022-05-20 00:45:44 -0300

    links to social media

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 function Footer(){
     return(
-        <div className='Footer'>
-            <h3>Footer</h3>
+        <div className='Footer container'>
+            <h3>Gabriel López</h3>
+            <a href="https://github.com/Gabs343"><FaGithub className='Icon'/></a>
+            <a href="https://www.linkedin.com/in/gabriellopez343/"><FaLinkedin className='Icon'/></a>
         </div>
     );
 }
